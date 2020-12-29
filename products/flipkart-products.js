de21a9583b64ff6f7067cca27c25c6fd948dcac9
Noamaan Mulla <noamaan.mulla03@gmail.com>
2020-12-29 23:47:49 +0530

    added category filter in product page

---
+++ products/flipkart-products.js
@@ -0,0 +1,18 @@
+let more_less = document.getElementsByClassName('more-less-button')[0];
+document.getElementsByClassName('more-less-display')[0].style.display = 'none';
+document.getElementsByClassName('more-less-button')[0].innerHTML = "Show 40+ More";
+
+more_less.onclick = () => {
+    if(document.getElementsByClassName('more-less-display')[0].style.display == 'none')
+        {
+            document.getElementsByClassName('more-less-display')[0].style.display = 'block';
+            document.getElementsByClassName('more-less-button')[0].innerHTML = "Show Less";
+    }
+    else
+        {
+            document.getElementsByClassName('more-less-display')[0].style.display = 'none';
+            document.getElementsByClassName('more-less-button')[0].innerHTML = "Show 40+ More";
+    }
+}
+
+// document.getElementsByClassName('more-less-button')[0].style.backgroundColor = "black";
